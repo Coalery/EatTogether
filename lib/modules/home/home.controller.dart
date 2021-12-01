b5429e97f46e4bff9226e145943e11de640bbf32
러리 <doralife12@naver.com>
2021-12-02 01:14:22 +0900

    Refactor: Remove unnecessary print

--- lib/modules/home/home.controller.dart
+++ lib/modules/home/home.controller.dart
@@ -28,12 +28,10 @@ class HomeController extends GetxController {
   Future<void> getPartyNear500m() async {
     isRefreshing(true);
     Position curPosition = await Geolocator.getCurrentPosition();
-    print(curPosition);
     dynamic response = await partyRepository.getPartyNear500m(
       curPosition.latitude,
       curPosition.longitude
     );
-    print(response);
     List<dynamic> rawData = List.from(response['data']);
     data = rawData.map((rawParty) => Party.fromJson(rawParty)).toList();
     isRefreshing(false);
