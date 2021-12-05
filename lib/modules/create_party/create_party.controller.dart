83a462c84081db777bb5e0fa409211c55db8df19
러리 <doralife12@naver.com>
2021-12-05 20:38:43 +0900

    Feat: Input page in create party page

--- lib/modules/create_party/create_party.controller.dart
+++ lib/modules/create_party/create_party.controller.dart
@@ -1,5 +1,33 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CreatePartyController extends GetxController {
-  
+  GlobalKey<FormState> formKey = GlobalKey();
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descsriptionController = TextEditingController();
+  TextEditingController restuarantController = TextEditingController();
+  TextEditingController goalPriceController = TextEditingController();
+
+  PageController pageController = PageController();
+
+  @override
+  void onClose() {
+    titleController.dispose();
+    descsriptionController.dispose();
+    restuarantController.dispose();
+    goalPriceController.dispose();
+    pageController.dispose();
+    super.onClose();
+  }
+
+  void onClickNext() {
+    if(formKey.currentState!.validate()) {
+      pageController.animateToPage(
+        1,
+        duration: Duration(milliseconds: 200),
+        curve: Curves.fastOutSlowIn
+      );
+    }
+  }
 }
--- lib/modules/create_party/create_party.page.dart
+++ lib/modules/create_party/create_party.page.dart
@@ -1,3 +1,4 @@
+import 'package:eat_together/common/constant.dart';
 import 'package:eat_together/modules/create_party/create_party.controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -7,7 +8,77 @@ class CreatePartyPage extends GetView<CreatePartyController> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(),
+        body: PageView(
+          controller: controller.pageController,
+          children: [
+            _TextInputPage()
+          ],
+        )
+      ),
+    );
+  }
+}
+
+class _TextInputPage extends GetView<CreatePartyController> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: controller.formKey,
+      child: Column(
+        children: [
+          Column(
+            children: [
+              Text('같이 먹을래의 제목'),
+              TextFormField(
+                controller: controller.titleController,
+                validator: (val) {
+                  if(val == null || val.isEmpty) return '제목을 입력해주세요.'; 
+                  return null;
+                },
+              ),
+              Text('같이 먹을래의 설명 (선택)'),
+              TextFormField(
+                controller: controller.descsriptionController,
+              ),
+              Text('시킬 가게'),
+              Text('누구나 가게를 특정할 수 있도록 써주세요! ex) OO동 OO치킨, OO마트 앞 OO버거'),
+              TextFormField(
+                controller: controller.restuarantController,
+                validator: (val) {
+                  if(val == null || val.isEmpty) return '시킬 가게를 입력해주세요.';
+                  return null;
+                },
+              ),
+              Text('목표 금액'),
+              TextFormField(
+                controller: controller.goalPriceController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  suffix: Text('원')
+                ),
+                validator: (val) {
+                  if(val == null || val.isEmpty) return '목표 금액을 입력해주세요.';
+                  
+                  int? parsedVal = int.tryParse(val);
+                  if(parsedVal == null) return '정수로 입력해주세요.';
+                  if(parsedVal < 0) return '양수를 입력해주세요.';
+
+                  return null;
+                },
+              )
+            ],
+          ),
+          TextButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Constant.mainColor)
+            ),
+            onPressed: controller.onClickNext,
+            child: Text(
+              '다음',
+              style: TextStyle(color: Colors.white),
+            )
+          )
+        ],
       ),
     );
   }
